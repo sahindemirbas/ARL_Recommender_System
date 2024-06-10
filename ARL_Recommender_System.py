7def22d9c05403626bbbcabd0791aa977efbce9c
Şahin Demirbaş <16301123032@ogr.bozok.edu.tr>
2024-06-11 00:08:37 +0300

    Update ARL_Recommender_System.py

--- ARL_Recommender_System.py
+++ ARL_Recommender_System.py
@@ -1,29 +1,3 @@
-# İŞ PROBLEMİ
-
-# Aşağıda 3 farklı kullanıcının sepet bilgileri verilmiştir. Bu sepet bilgilerine en uygun ürün önerisini birliktelik
-# kuralı kullanarak yapınız. Ürün önerileri 1 tane ya da 1'den fazla olabilir. Karar kurallarını 2010-2011 Germany
-# müşterileri üzerinden türetiniz. Kullanıcı 1’in sepetinde bulunan ürünün id'si: 21987 Kullanıcı 2’in sepetinde bulunan
-# ürünün id'si : 23235 Kullanıcı 3’in sepetinde bulunan ürünün id'si : 22747
-
-# Veri Seti Hikayesi
-
-# Online Retail II isimli veri seti İngiltere merkezli bir perakende şirketinin 01/12/2009 - 09/12/2011 tarihleri
-# arasındaki online satış işlemlerini içeriyor. Şirketin ürün kataloğunda hediyelik eşyalar yer almaktadır ve çoğu
-# müşterisinin toptancı olduğu bilgisi mevcuttur.
-
-# Değişkenler
-
-# InvoiceNo = Fatura Numarası ( Eğer bu kod C ile başlıyorsa işlemin iptal edildiğini ifade eder )
-# StockCode = Ürün kodu ( Her bir ürün için eşsiz )
-# Description = Ürün ismi
-# Quantity = Ürün adedi ( Faturalardaki ürünlerden kaçar tane satıldığı)
-# InvoiceDate = Fatura tarihi
-# UnitPrice = Fatura fiyatı ( Sterlin )
-# CustomerID = Eşsiz müşteri numarası
-# Country = Ülke ismi
-
-# Proje Görevleri
-
 # Görev 1: Veriyi Hazırlama
 
 # Adım 1: Online Retail II veri setinden 2010-2011 sheet’ini okutunuz.
